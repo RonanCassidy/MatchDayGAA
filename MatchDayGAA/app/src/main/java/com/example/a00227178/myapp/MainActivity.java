3e0813eb5bba773bc7b7d7a71962e97ebb5b89bc
RonanCassidy <rcassidydev@gmail.com>
2016-03-01 15:00:26 +0000

    First commit of project that was on college pc for home use

---
+++ MatchDayGAA/app/src/main/java/com/example/a00227178/myapp/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.a00227178.myapp;
+
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.twitter.sdk.android.tweetcomposer.ComposerActivity;
+import com.twitter.sdk.android.tweetcomposer.TweetComposer;
+
+import java.io.Console;
+import java.net.URI;
+
+import io.fabric.sdk.android.Fabric;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "21ezbECEwOrCca0ntNnmzB9p1";
+    private static final String TWITTER_SECRET = "J4UzGX3uVNDuWCFT74KOHkCgfOIC9p4cTEvEypmiF7o9m8n6da";
+
+    TwitterLoginButton loginButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig),new TweetComposer());
+        setContentView(R.layout.activity_main);
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                String UserName = result.data.getUserName();
+                Toast.makeText(MainActivity.this, UserName, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                // Do something on failure
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result to the login button.
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public void btnClick(View v)
+    {
+        boolean twitterInstalled = false;
+
+        try{
+            ApplicationInfo info = getPackageManager().
+                    getApplicationInfo("com.twitter.android", 0 );
+            twitterInstalled = true;
+        }
+        catch( PackageManager.NameNotFoundException e )
+        {
+        }
+        System.out.println("hellooooo " + twitterInstalled);
+        if(twitterInstalled) {
+            TweetComposer.Builder builder = new TweetComposer.Builder(this)
+                    .text("@A00227178 this is placeholder text");
+            builder.show();
+        }
+        else{
+
+            final TwitterSession session = TwitterCore.getInstance().getSessionManager()
+                    .getActiveSession();
+            System.out.println("hellooooo "+session);
+            final Intent intent = new ComposerActivity.Builder(MainActivity.this)
+                    .session(session)
+                    .createIntent();
+            startActivity(intent);
+        }
+    }
+    public void goSquadActivity(View v)
+    {
+        Intent intent = new Intent(this, SquadBuilder.class);
+        startActivity(intent);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ MatchDayGAA/app/src/main/java/com/example/a00227178/myapp/SquadBuilder.java
@@ -0,0 +1,182 @@
+package com.example.a00227178.myapp;
+
+import android.app.ActionBar;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.internal.widget.AdapterViewCompat;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class SquadBuilder extends ActionBarActivity {
+
+    String[] squad = {"Ronan Cassidy","Tom Devery","Johnny Kenny","Georgie Digan", "Darren Kelly","Colin Kenny","Colm Kelly","Pat Brennan",
+            "Jordan Drennan","Steven Thompson","Billy Dunican", "Joe Kenny","Oisin Kelly","David Kelly","Ronan McGuire","Simon Goodman","B.Kenny"};
+    ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_squad_builder);
+
+        ListView listView = (ListView) findViewById(R.id.listview);
+        final ArrayList<String> list = new ArrayList<String>();
+
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, squad);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+        registerForContextMenu(listView);
+    }
+
+
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)
+    {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Select " +adapter.getItem(info.position) + " Position");
+        menu.add(0, v.getId(), 1, "1: Goalkeeper");//groupId, itemId, order, title
+        menu.add(0, v.getId(), 2, "2: Right-Corner Back");
+        menu.add(0, v.getId(), 3, "3: Full-Back");
+        menu.add(0, v.getId(), 4, "4: Left-Corner Back");
+        menu.add(0, v.getId(), 5, "5: Right-Half Back");
+        menu.add(0, v.getId(), 6, "6: Centre-Half Back");
+        menu.add(0, v.getId(), 7, "7: Left-Half Back");
+        menu.add(0, v.getId(), 8, "8: Midfield-1");
+        menu.add(0, v.getId(), 9, "9: Midfield-2");
+        menu.add(0, v.getId(), 10, "10: Right-Half Forward");
+        menu.add(0, v.getId(), 11, "11: Centre Forward");
+        menu.add(0, v.getId(), 12, "12: Left-Half Forward");
+        menu.add(0, v.getId(), 13, "13: Right-Corner Forward");
+        menu.add(0, v.getId(), 14, "14: Full-Forward");
+        menu.add(0, v.getId(), 15, "15: Left-Corner Forward");
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+        int menuItemTitle = item.getOrder();
+
+
+        String listItemName = squad[info.position];
+        TextView tv1 = getTextView(menuItemTitle);
+        tv1.setText(listItemName);
+        Toast.makeText(getApplicationContext(),listItemName+ " is now at " + menuItemTitle,Toast.LENGTH_LONG).show();
+
+        return true;
+    }
+    private void takeScreenshot() {
+        Date now = new Date();
+        android.text.format.DateFormat.format("yyyy-MM-dd", now);
+
+        try {
+            // image naming and path  to include sd card  appending name you choose for file
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
+            mPath  = mPath.replaceAll("[|?*<\":>+\\[\\]/']", "_");
+            // create bitmap screen capture
+            View v1 = getWindow().getDecorView().getRootView();
+            v1.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+            v1.setDrawingCacheEnabled(false);
+
+            File imageFile = new File(mPath);
+
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            //openScreenshot(imageFile);
+        } catch (Throwable e) {
+            // Several error may come out with file handling or OOM
+            e.printStackTrace();
+        }
+    }
+    private void openScreenshot(File imageFile) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        Uri uri = Uri.fromFile(imageFile);
+        intent.setDataAndType(uri, "image/*");
+        startActivity(intent);
+    }
+    public TextView getTextView(int pos)
+    {
+        TextView tv =null;
+        if(pos == 1)
+            tv = (TextView)findViewById(R.id.goalkeeper);
+        else if(pos == 2)
+            tv = (TextView)findViewById(R.id.rcb);
+        else if(pos == 3)
+            tv = (TextView)findViewById(R.id.fullback);
+        else if(pos == 4)
+            tv = (TextView)findViewById(R.id.lcb);
+        else if(pos == 5)
+            tv = (TextView)findViewById(R.id.rhb);
+        else if(pos == 6)
+            tv = (TextView)findViewById(R.id.center_back);
+        else if(pos == 7)
+            tv = (TextView)findViewById(R.id.lhb);
+        else if(pos == 8)
+            tv = (TextView)findViewById(R.id.midfield1);
+        else if(pos == 9)
+            tv = (TextView)findViewById(R.id.midfield2);
+        else if(pos == 10)
+            tv = (TextView)findViewById(R.id.rhf);
+        else if(pos == 11)
+            tv = (TextView)findViewById(R.id.centre_forward);
+        else if(pos == 12)
+            tv = (TextView)findViewById(R.id.lhf);
+        else if(pos == 13)
+            tv = (TextView)findViewById(R.id.rff);
+        else if(pos == 14)
+            tv = (TextView)findViewById(R.id.full_forward);
+        else if(pos == 15)
+            tv = (TextView)findViewById(R.id.lff);
+
+        return tv;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_squad_builder, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            takeScreenshot();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
